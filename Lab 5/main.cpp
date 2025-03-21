28af42d9037aee621c11562bafb0a20e1d930561
Andrew81391 <79299955+Andrew81391@users.noreply.github.com>
2025-03-21 15:01:41 -0600

    fixed the task reading ability.

--- Lab 5/main.cpp
+++ Lab 5/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -17,6 +18,18 @@ public:
         : ID(id), exec_time(e), period(p), release_time(r), is_periodic(periodic) {}
 };
 
+void parseTaskLine(const string& line, string& id, int32_t& first, int32_t& second) {
+    istringstream ss(line);
+    string token;
+
+    getline(ss, token, ','); // ID
+    id = token;
+    getline(ss, token, ','); // Exec time
+    first = stoi(token);
+    getline(ss, token, ','); // Period or Release time
+    second = stoi(token);
+}
+
 void readInputFile(const string& filename, vector<taskPeriodic>& periodic_tasks, vector<taskPeriodic>& aperiodic_tasks, int& sim_time) {
     ifstream infile(filename);
     if (!infile) {
@@ -27,39 +40,29 @@ void readInputFile(const string& filename, vector<taskPeriodic>& periodic_tasks,
     int num_tasks;
     infile >> num_tasks;
     infile >> sim_time;
-
     string line;
-    getline(infile, line); // consume newline
+    getline(infile, line); // eat newline
 
     // Read periodic tasks
     for (int i = 0; i < num_tasks; i++) {
         getline(infile, line);
-        istringstream ss(line);
         string id;
         int exec_time, period;
-        char comma;
-
-        ss >> id >> comma >> exec_time >> comma >> period;
+        parseTaskLine(line, id, exec_time, period);
         periodic_tasks.emplace_back(id, exec_time, period, 0, true);
     }
 
-    // Read aperiodic tasks if present
     int num_aperiodic_tasks = 0;
-    if (infile >> num_aperiodic_tasks) {
-        getline(infile, line); // consume newline
+    if (getline(infile, line) && !line.empty() && isdigit(line[0])) {
+        num_aperiodic_tasks = stoi(line);
         for (int i = 0; i < num_aperiodic_tasks; i++) {
             getline(infile, line);
-            istringstream ss(line);
             string id;
             int exec_time, release_time;
-            char comma;
-
-            ss >> id >> comma >> exec_time >> comma >> release_time;
+            parseTaskLine(line, id, exec_time, release_time);
             aperiodic_tasks.emplace_back(id, exec_time, 0, release_time, false);
         }
     }
-
-    infile.close();
 }
 
 int main(int argc, char* argv[]) {
@@ -74,8 +77,8 @@ int main(int argc, char* argv[]) {
 
     readInputFile(argv[1], periodic_tasks, aperiodic_tasks, sim_time);
 
-    // Print loaded tasks
     cout << "Simulation Time: " << sim_time << " ms\n";
+
     cout << "Periodic Tasks:\n";
     for (const auto& task : periodic_tasks) {
         cout << task.ID << ": Exec Time = " << task.exec_time << ", Period = " << task.period << "\n";
